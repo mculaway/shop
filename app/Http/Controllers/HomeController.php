1064a727adf073dc510acb8416bdd9676c73e6a7
mculaway <92026862+mculaway@users.noreply.github.com>
2023-05-17 22:59:58 +0800

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $admin = false;
+        // get all the products
+        $products = Product::all();
+
+
+        //pass the data to th view
+        return view('home', ['products' => $products, 'admin' => $admin],);
+    }
+}
